f672b72c97fc057586705bc0e4cca7d0b3ebb9a9
LightCrimson <quangminhjean@gmail.com>
2023-06-02 10:11:54 +0200

    Test6

---
+++ main.js
@@ -0,0 +1,4 @@
+ // XSS test on title using url params (?user="")
+ var urlParams = new URLSearchParams(window.location.search);
+ var user = urlParams.get('user');
+ document.getElementById("title").innerHTML = `Hello ${user}`;
